e62d2b6c47b35f22a8f0c87e37637c15ec705e0a
sahilkhurana19 <sahil_khurana@outlook.com>
2021-03-28 23:10:58 +0530

    Added font awesome

--- src/background.js
+++ src/background.js
@@ -1,22 +1,12 @@
 let blockedSites = [];
 
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.get('blockedSites', function(data) {
-        if (!data.blockedSites) {
-            chrome.storage.sync.set({'blockedSites': []}, function() {
-                console.log("Initialised blocked sites memory");
-            });
-        } else {
-            blockedSites = data.blockedSites;
-            console.log(blockedSites);
-        }
-    })
+    fetchBlockedSitesList();
 });
 
-// chrome.webRequest.onBeforeRequest.addListener(
 chrome.tabs.onUpdated.addListener(
     function(tabId, changeInfo, tab) {
-        console.log(...arguments);
+        fetchBlockedSitesList();
         if (changeInfo && changeInfo.status === "loading") {
             let url = new URL(tab.url);
             if (blockedSites && blockedSites.indexOf(url.host) != -1) {
@@ -28,17 +18,15 @@ chrome.tabs.onUpdated.addListener(
     }
 );
 
-// chrome.webNavigation.onCommitted.addListener((data) => {
-//     let websiteUrl = data.url;
-//     let urlToBlock = new URL(websiteUrl);
-//     let urlOrigin = urlToBlock.origin;
-//     chrome.storage.sync.get('blockedSites', function(data) {
-//         blockedSites = data.blockedSites;
-//         console.log(blockedSites);
-//         if (blockedSites) {
-//             if (blockedSites.indexOf(urlOrigin) > -1) {
-//                 window.location = "https://www.google.com"
-//             }
-//         }
-//     })
-// })
+function fetchBlockedSitesList() {
+    chrome.storage.sync.get('blockedSites', function(data) {
+        if (!data.blockedSites) {
+            chrome.storage.sync.set({'blockedSites': []}, function() {
+                console.log("Initialised blocked sites memory");
+            });
+        } else {
+            blockedSites = data.blockedSites;
+            console.log(blockedSites);
+        }
+    })
+}
--- src/options/options.js
+++ src/options/options.js
@@ -1,9 +1,52 @@
 function fetchBlockedSites() {
     chrome.storage.sync.get('blockedSites', function(data) {
         let listDom = document.getElementById("blocked-sites-list");
-        listDom.innerText = data.blockedSites;
+        if (data.blockedSites) {
+            data.blockedSites.forEach(element => {
+                let siteRow = document.createElement("div")
+                customiseSiteRow(siteRow);
+
+                let siteNameCol = document.createElement("div")
+                customiseSiteNameCol(siteNameCol);
+
+                let siteName = document.createElement("span")
+
+                let deleteButtonCol = document.createElement("div")
+                customiseDeleteButtonCol(deleteButtonCol);
+
+                let deleteButton = document.createElement("button")
+                customiseDeleteButton(deleteButton)
+
+                siteName.innerText = element;
+
+                siteNameCol.appendChild(siteName);
+                deleteButtonCol.appendChild(deleteButton);
+
+                siteRow.appendChild(siteNameCol);
+                siteRow.appendChild(deleteButtonCol);
+
+                listDom.appendChild(siteRow);
+            });
         console.log(data);
+        }
     })
+}
+
+function customiseSiteRow(siteRow) {
+    siteRow.classList = "row";
+}
+
+function customiseSiteNameCol(siteNameCol) {
+    siteNameCol.classList = "col-3";
+}
+
+function customiseDeleteButtonCol(deleteButtonCol) {
+    deleteButtonCol.classList = "col-3";
+}
+
+function customiseDeleteButton(deleteButton) {
+    deleteButton.innerHTML = `<i class="fa fa-trash"></i>`;
+    deleteButton.classList = "btn btn-danger";
 }
 
 document.getElementById("clear-memory-button").onclick = function clearMemory() {
