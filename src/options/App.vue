7eabe675338c927462e3639d6e2113a8493a9918
Sahil Khurana <sahil@karmacircles.com>
2021-08-14 23:07:51 +0530

    Added options page scss

--- src/options/App.vue
+++ src/options/App.vue
@@ -1,22 +1,46 @@
 <template>
   <div>
-    <hello-world />
-    asdasd
+    {{ text }}
   </div>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue'
+import { Component, Vue } from "vue-property-decorator";
 
-export default {
-  name: 'App',
-  components: { HelloWorld }
+@Component({
+    name: "options-page"
+})
+export default class OptionsPage extends Vue {
+    text = "Text"
+
 }
+
 </script>
 
-<style>
-html {
-  width: 400px;
-  height: 400px;
-}
+<style lang="scss">
+    .blocked-sites-container {
+        border-right: 1px solid black;
+        min-height: 60vh;
+
+        .site-row {
+            margin-top: 5px;
+            margin-bottom: 5px;
+            .delete-button-container {
+                text-align: right;
+            }
+        }
+    }
+
+    .hidden {
+        display: none !important;
+    }
+
+    #version-text {
+        font-size: 10px;
+        text-align: right;
+        position: absolute;
+        right: 0;
+        top: 0;
+        height: 0px;
+    }
 </style>
