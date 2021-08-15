72c198b9d9ca410d7fcb16b873339ad3ecc0a038
Sahil Khurana <sahil@karmacircles.com>
2021-08-16 00:42:32 +0530

    Added background script

--- src/background.js
+++ src/background.js
@@ -1,3 +1,32 @@
-// browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-//   console.log('Hello from the background')
-// })
+let blockedSites = [];
+
+chrome.runtime.onInstalled.addListener(() => {
+    fetchBlockedSitesList();
+});
+
+chrome.tabs.onUpdated.addListener(
+    function(tabId, changeInfo, tab) {
+        fetchBlockedSitesList();
+        if (changeInfo && changeInfo.status === "loading" && tab.url) {
+            let url = new URL(tab.url);
+            if (blockedSites && blockedSites.indexOf(url.host) != -1) {
+                chrome.tabs.query({currentWindow: true, active: true}, function (tab) {
+                    chrome.tabs.update(tab.id, {url: `chrome-extension://${chrome.runtime.id}/options.html`});
+                });
+            }
+        }
+    }
+);
+
+function fetchBlockedSitesList() {
+    chrome.storage.sync.get('blockedSites', function(data) {
+        if (!data.blockedSites) {
+            chrome.storage.sync.set({'blockedSites': []}, function() {
+                console.log("Initialised blocked sites memory");
+            });
+        } else {
+            blockedSites = data.blockedSites;
+            console.log(blockedSites);
+        }
+    })
+}
