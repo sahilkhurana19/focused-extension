57cf212a11f7dd3f73630442c5dbc1b7dedc93be
Sahil Khurana <sahil@karmacircles.com>
2021-08-17 23:42:41 +0530

    Ported options screen to vue

--- src/options/App.vue
+++ src/options/App.vue
@@ -1,7 +1,66 @@
 <template>
-  <div>
-    {{ text }}
-  </div>
+    <div class="container">
+        <div class="row mt-2">
+            <div id="version-text">
+                Version - 0.1.0
+            </div>
+            <div class="col text-center">
+                <h2>Focused Extension</h2>
+                <h4>Block sites and get working!</h4>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-6 blocked-sites-container">
+                <div class="row">
+                    <div class="col text-center">
+                        <h5>Unblock currently blocked websites</h5>
+                    </div>
+                </div>
+                <hr/>
+                <div class="row" v-if="blockedSites.length > 0">
+                    <div class="col">
+                        <div id="blocked-sites-list">
+                            <div v-for="(site, index) in blockedSites" :key="index">
+                                <div class="row site-row">
+                                    <div class="col-10 site-name-container">
+                                        {{ site }}
+                                    </div>
+                                    <div class="col-2 delete-button-container">
+                                        <b-button variant="danger" @click="removeWebsiteFromList(site)"><b-icon-trash-fill/></b-button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div v-else class="row" id="empty-list-container">
+                    <div class="col text-center">
+                        <p>You'll be able to track your blocked websites here!</p>
+                    </div>
+                </div>
+                <div class="row" v-if="blockedSites.length > 0">
+                    <div class="col">
+                        <div id="clear-memory-button" class="text-center" @click="clearMemory()">
+                            <b-button variant="danger">Reset Blocked Sites List</b-button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="row">
+                    <div class="col text-center">
+                        <h5>Pomodoro Timer Settings</h5>
+                    </div>
+                </div>
+                <hr/>
+                <div class="row">
+                    <div class="col text-center">
+                        <p>Feature coming soon!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -11,8 +70,40 @@ import { Component, Vue } from "vue-property-decorator";
     name: "options-page"
 })
 export default class OptionsPage extends Vue {
-    text = "Text"
+    blockedSites = [];
 
+    mounted() {
+        this.fetchBlockedSites();
+    }  
+
+    fetchBlockedSites() {
+        chrome.storage.sync.get('blockedSites', (data) => {
+            this.blockedSites = data.blockedSites || [];
+        })
+    }
+
+    removeWebsiteFromList(url) {
+        chrome.storage.sync.get('blockedSites', (data) => {
+            if (data && data.blockedSites && data.blockedSites.length > 0) {
+                let blockedSites = data.blockedSites;
+                let newList = blockedSites.filter((element) => element !== url);
+                this.storeNewList(newList);
+            }
+        })
+    }
+
+    storeNewList(newList) {
+        chrome.storage.sync.set({'blockedSites': newList}, () => {
+            this.blockedSites = newList
+        })
+    }
+
+    clearMemory() {
+        chrome.storage.sync.clear();
+        chrome.storage.sync.get('blockedSites', (data) => {
+            this.blockedSites = data.blockedSites || [];
+        })
+    }
 }
 
 </script>
@@ -29,10 +120,6 @@ export default class OptionsPage extends Vue {
                 text-align: right;
             }
         }
-    }
-
-    .hidden {
-        display: none !important;
     }
 
     #version-text {
